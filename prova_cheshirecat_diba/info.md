8f9526105efd3f79dcc5aac44f0875cb0160fc71
AlessioDiBattista <alessio.diba.08@gmail.com>
2026-04-12 18:34:39 +0200

    finito video 15 con prove sul cheshire cat di michael saccone, aggiunto nuovo plugin only_docs dove ci sono 2 funzioni di hook

--- prova_cheshirecat_diba/info.md
+++ prova_cheshirecat_diba/info.md
@@ -17,3 +17,13 @@ usare "/bye" per uscire dall'uso del modello
 
 PER INSTALLARE LE DEPENDENCIES E LE LIBRERIE VARIE, CREARE UN requirements.text DENTRO LA CARTELLA DEL PLUGIN,
 LO STREGATTO INSTALLERA' AUTOMATICAMENTE LE LIBRERIE IMPORTATE NEGLI SCRIPT
+
+
+
+dopo aver fatto partire "ollama serve" per il server ollama e "ollama run nome-modello" per il modello in esecuzione, nello
+stregatto impostare come indirizzo al server ollama (URL modello) http://host.internal.docker:11434
+
+
+
+
+Usato Qdrant FastEmbed come embedder dell'agente e ollama per l'LLM (poi bisogna vedere quale modello è stato messo su ollama)
--- prova_cheshirecat_diba/plugins/only_docs/main.py
+++ prova_cheshirecat_diba/plugins/only_docs/main.py
@@ -1,6 +1,6 @@
 from cat.mad_hatter.decorators import hook
 
-@hook # priorità predefinits (0)
+@hook # priorità predefinits (1)
 # Funzione che si chiama esattamente come l'hook
 def before_cat_sends_message(message, cat):
     # Stamperà un messaggio con la seguente struttura:
@@ -19,8 +19,23 @@ def before_cat_sends_message(message, cat):
     #         }
     #     }
     # }
+
+    memorie_dichiarative_gatto = cat.working_memory.declarative_memories # Analogo di message.why.memory["declarative"]
     
     print(message.why.memory["declarative"])
     if len(message.why.memory["declarative"]) == 0: # Tag della memoria dichiarativa vuota quindi con valore 0 
         message.content = "Non lo posso sapere" # message.content è il messaggio finale che esprime il gatto
+    # bisogna sempre restituire il valore perché se ci sono più hooks vengono messi in sequenza (pipeline) e quindi devono restituire un dato
     return message
+
+    # In questo modo la risposta del gatto sarà modificata nel caso in cui il messaggio non sia prodotto di una richiesta con
+    # una memoria dichiarativa rilevante (in questo caso)
+
+
+# metodo di hook per considerare la memoria episodica in modo diverso (cambiamento valore di K)
+@hook # priorità di default = 1
+def before_cat_recalls_episodic_memories(episodic_recall_config, cat):
+    # cambiamento del numero di memorie episodiche richiamate
+    episodic_recall_config["k"] = 0
+
+    return episodic_recall_config 
