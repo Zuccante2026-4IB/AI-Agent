85a49b22678498c9ee9385a0fd96c4468cbd22a6
AlessioDiBattista <alessio.diba.08@gmail.com>
2026-04-14 15:40:29 +0200

    aggiunti commenti TODO e TO TEST, iniziato sviluppo del metodo per caricare le impostazioni del plugin con gestione dell'errore ValidationError causato dai decoratori @field_validator dentro settings.py, inc aso di errore si manda una notifica di errore nella UI con il messaggio di errore

--- kith/plugins/ricerca_eventi/main.py
+++ kith/plugins/ricerca_eventi/main.py
@@ -1,1 +1,11 @@
-#script main
+# script main
+
+from pydantic import ValidationError
+
+# TODO DA MODIFICARE IN BASE A QUANDO VA CHIAMATO
+#TO TEST
+def carica_impostazioni_plugin():
+    try:
+        settings = cat.mad_hatter.get_plugin().load_settings()
+    except ValidationError as _: # "_" indica quando l'istanza non si usa (sennò si sarebbe messo "e" al posto di "_")
+        cat.send_ws_message("Campo non valido", "error") # messaggio all'utente indicando che c'è un'errore
