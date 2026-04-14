6809aa96f33b7c6a3b3e5c54829405f1a7a150ea
AlessioDiBattista <alessio.diba.08@gmail.com>
2026-04-14 15:44:49 +0200

    aggiunti commenti del codice e un TO TEST

--- kith/plugins/personalita/main.py
+++ kith/plugins/personalita/main.py
@@ -1,9 +1,9 @@
 # Script main
 from cat.mad_hatter.decorator import hook
 
-#TEST
+#TO TEST
 
-#caricare
+# funzione per caricare il prompt prefix
 @hook(priority = 10) # default priority = 1 --> priorità bassa
 def agent_prompt_prefix(prefix, cat):
     settings = cat.mad_hatter.get_plugin().load_settings() # carica le informazioni dall'oggetto creato in settings.py
