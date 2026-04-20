3fb3187720a9e1e0c0f0ca026740e719cbdcae09
AlessioDiBattista <alessio.diba.08@gmail.com>
2026-04-20 15:36:27 +0200

    update

--- kith/plugins/ricerca_eventi/ricerca_eventi.py
+++ kith/plugins/ricerca_eventi/ricerca_eventi.py
@@ -3,71 +3,36 @@
 from cat.mad_hatter.decorators import tool
 
 
-#* se la ricerca richiede più parametri, si possono mettere in sequenza le funzioni di ricerca
+# se la ricerca richiede più parametri, si possono mettere in sequenza le funzioni di ricerca
 
 @tool(
     return_direct=False, # per indicare che la risposta non va stampata direttamente ma "rimandata" al modello che la elabora
     examples=["", ""]
-) # TODO AGGIUNGERE ESEMPI PER AUMENTARE LA PRECISIONE DI INERENZA COL TOOL PER CERCARE GLI EVENTI
-# TODO CREARE FIRME PER CERCARE IN BASE A QUALE PARAMETRO CERCARE (RICHIESTO DALL'UTENTE)
-def cerca_con_parametro(param_luogo, cat):
-    #TODO FORNIRE PRECISA DOCUMETNAZIONE DEL METODO E DEL CASO IN CUI USARLO E CHE PARAMETRI PRENDERE
+)
+#? GUARDARE SE USARE UN PARAMETRO SINGOLO OPPURE UN KWARGS SE CI SONO PIU' PARAMETRI
+def cerca_con_parametro(param, cat):
     """
         Funzione per cercare in modo parametrico nel database gli eventi secondo ciò che chiede l'utente.
+        L'input è il criterio secondo il quale l'utente vuole cercare che sia:
+        - il nome dell'evento
+        - la sua data
+        - la sua durata
+        - il luogo dove si svolge
+        - la sua descrizione
+        - i tag che contiene che indicano un pò "l'argomento" dell'evento
+        - il target che ha nel pubblico in base all'età
+        - l'id dell'organizzatore dell'evento
+        - rank dell'evento quindi la sua valutazione
         
+        
+        TODO CREARE FORMATTAZIONE DELLA RISPOSTA E IN BASE A QUELLA FORMARE UNA QUERY OPPURE FAR FORMARE LA QUERY ALL'LLM
     """
-    
+    pass
+
     db_address = "" # API di strapi per connettersi al backend e poi al database
+    db_query = "" # query per l'API di strapi per la ricerca mirata
     
-    pass #* da non considerare la funzione finché non è completa
 
-@tool(
-    return_direct=False,
-    examples=["", ""]
-)
-def cerca_con_parametro(param_data, cat):
-    """
-        Descrizione della funzione...
-    """
-    
-    pass
-
-@tool(
-    return_direct=False,
-    examples=["", ""]
-)
-def cerca_con_parametro(param_orario, cat):
-    """
-        Descrizione della funzione...
-    """
-    
-    pass
-
-
-@tool(
-    return_direct=False,
-    examples=["", ""]
-)
-def cerca_con_parametro(param_target, cat):
-    """
-        Descrizione della funzione...
-    """
-    
-    pass
-
-@tool(
-    return_direct=False,
-    examples=["", ""]
-)
-def cerca_con_parametro(param_tag, cat):
-    """
-        Descrizione della funzione...
-    """
-    
-    pass
-
-
-#TODO IMPLEMENTARE RICERCA PER DESCRIZIONE (VEDI GRUPPO BACKEND COME FORNISCE LE DESCRIZIONI)
 
 
 @tool(
@@ -76,7 +41,7 @@ def cerca_con_parametro(param_tag, cat):
 )
 def ottieni_informazioni_evento(evento, cat):
     """
-        Funzione per...
+        Funzione per... (AGGIUNGERE DESCRIZIONE)
     """
     
     pass
