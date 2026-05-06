f05e7fa4d19064212d37e6c648a7ed0e83ffe6d4
AlessioDiBattista <alessio.diba.08@gmail.com>
2026-05-06 09:32:41 +0200

    continuato sviluppo della ricerca per provare a ricevere una risposta dal server

--- kith/plugins/ricerca_eventi/ricerca_eventi.py
+++ kith/plugins/ricerca_eventi/ricerca_eventi.py
@@ -11,7 +11,7 @@ import json
     return_direct=False, # per indicare che la risposta non va stampata direttamente ma "rimandata" al modello che la elabora
     examples=["", ""] #todo
 )
-
+#TO TEST
 def cerca_con_parametro(*param, cat):
     # L'attributo *param indica tutti i criteri secondo cui si puo fare la ricerca
     """
@@ -31,11 +31,13 @@ def cerca_con_parametro(*param, cat):
             - giovani_adulti (gente compresa tra i 25 e i 35 anni)
             
             Non accetti altre categorie oltre a queste 4 e devi inserire nella posizione 0
-            del parametro *param (kwarg) la seguente stringa adattandola alla richiesta dell'utente
-            inserendo al posto di NOME_CATEGORIA il valore desiderato capendolo dall'età che richiede
-            o dalla categoria
+            del parametro *param (kwarg) una lista con dentro per ogni valore i valori richiesti
+            quindi se chiede più categorie tu fai una lista con ognuna di queste sennò una lista
+            con un valore solo
             
-            filters[target][fascia][$eq]=NOME_CATEGORIA
+            esempio:
+                Se l'utente chiede giovani e adolescenti tu crei una lista in
+                param[0] equivalente a ['giovani', 'adolescenti']
             
         - l'id dell'organizzatore dell'evento
         - rank dell'evento quindi la sua valutazione
@@ -49,8 +51,29 @@ def cerca_con_parametro(*param, cat):
     # Content-Type = application/json
     BASE_URL = "https://strapi.brusegan.it"
     BEARER_TOKEN = "cff92e74316f57f7cd63ce9f93cf8fb309f0f15f673ed41d81afe4f1569a81f88d6b1b4268a94f97e5eb52802a1e1b49ac6702c060f1b96d1d02fa3103f84df65445e2307cd5f15b3ccb141fc91147470465304d44d6f53784989b971c4468aa6932c0b9dc3ed37da4a33e2cd58fcb9fdb87863ead235adca7c87513f47f6e1c"
+    LISTA_FASCE = [
+        "preadolescenti",
+        "adolescenti",
+        "giovani",
+        "giovani_adulti"
+    ]
     
-    response = requests.get(f"{BASE_URL}/api/eventos?populate=*")
+    filtri_ricerca = param[0]
+    
+    query_params = {
+        "populate": "*", # permette di avere gli eventi con tutti i dettagli
+    }
+    
+    #popolamento dell'attributo filters[target][fascia][$in][{i}] in params con i valori corretti
+    for i, f in enumerate(LISTA_FASCE):
+        query_params[f"filters[target][fascia][$in][{i}]"] = f #
+    
+    query_headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {BEARER_TOKEN}"
+    }
+    
+    response = requests.get(f"{BASE_URL}/api/eventos", params=query_params, headers=query_headers)
     
     if response.status_code == 200:
         dati = response.json()
