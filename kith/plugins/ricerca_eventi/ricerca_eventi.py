d02b6e75c0aab192bc2df79373263f2c3e822051
AlessioDiBattista <alessio.diba.08@gmail.com>
2026-05-08 17:53:46 +0200

    impostati examples e doc per il tool di ricerca -- da revisionare

--- kith/plugins/ricerca_eventi/ricerca_eventi.py
+++ kith/plugins/ricerca_eventi/ricerca_eventi.py
@@ -9,44 +9,81 @@ import json
 
 @tool(
     return_direct=False, # per indicare che la risposta non va stampata direttamente ma "rimandata" al modello che la elabora
-    examples=["", ""] # todo 
+    examples=[
+    # età esplicita propria
+    "Ho 12 anni, ci sono eventi per me?",
+    "Ho 16 anni, cosa posso fare questo weekend?",
+    "Ho 21 anni, quali eventi trovo?",
+    "Ho 28 anni, cosa mi consigli?",
+    "Ho 10 anni e voglio uscire, cosa c'è?",
+    "Ho 35 anni, ci sono eventi adatti a me?",
+
+    # età esplicita di terzi
+    "Mia figlia ha 11 anni, ci sono attività per lei?",
+    "Mio fratello ha 17 anni, cosa può fare?",
+    "Cerco eventi per mio figlio che ha 13 anni",
+    "Ho un gruppo di ragazzi di 15 anni, cosa organizziamo?",
+
+    # categorie nominate direttamente
+    "Ci sono eventi per adolescenti?",
+    "Cosa c'è per i giovani adulti?",
+    "Trovami eventi per preadolescenti",
+    "Quali attività ci sono per i giovani?",
+    "Dammi una lista di eventi per giovani adulti",
+
+    # range d'età descrittivo
+    "Siamo un gruppo tra i 18 e i 24 anni",
+    "Cerco attività per ragazzi tra i 10 e i 13 anni",
+    "Ho tra i 25 e i 35 anni, cosa c'è?",
+    "Attività per chi ha tra i 14 e i 17 anni",
+
+    # formulazioni implicite / alias
+    "Sono maggiorenne da poco, che eventi ci sono?",
+    "Sono under 18, cosa posso fare?",
+    "Attività per under 14 nel weekend",
+    "Cosa fanno i giovani in città?",
+    "Sono quasi adulto, ci sono eventi per me?",
+    "Cerco qualcosa per i giovanissimi",
+    "Cosa c'è per i ragazzi delle medie?",
+    "Attività per ragazzi delle superiori",
+    "Cose da fare per chi è appena entrato nel mondo del lavoro",
+    "Sono uno studente universitario, cosa c'è in programma?",
+
+    # richieste generiche con contesto d'età
+    "Voglio uscire stasera, ho 19 anni",
+    "Cosa c'è di bello da fare per un 30enne?",
+    "Suggeriscimi un evento, ho 14 anni",
+    "Cosa organizzate per i più giovani?",
+    "Attività per ragazzi in età scolare",
+]
 )
 # TO TEST 
 def cerca_con_parametro(*param, cat):
     # L'attributo *param indica tutti i criteri secondo cui si puo fare la ricerca
     """
-        Funzione per cercare in modo parametrico nel database gli eventi secondo ciò che chiede l'utente.
-        L'input è il criterio secondo il quale l'utente vuole cercare che sia:
-        
-        - il nome dell'evento
-        - la sua data
-        - la sua durata
-        - il luogo dove si svolge
-        - la sua descrizione
-        - i tag che contiene che indicano un pò "l'argomento" dell'evento
-        - il target che ha nel pubblico in base all'età scegliendo tra:
-            - preadolescenti (gente compresa tra i 10 e i 13 anni)
-            - adolescenti (gente compresa tra i 14 e i 17 anni)
-            - giovani (gente compresa tra i 18 e i 24 anni)
-            - giovani_adulti (gente compresa tra i 25 e i 35 anni)
-            
-            Non accetti altre categorie oltre a queste 4 e devi inserire nella posizione 0
-            del parametro *param (kwarg) una lista con dentro per ogni valore i valori richiesti
-            quindi se chiede più categorie tu fai una lista con ognuna di queste sennò una lista
-            con un valore solo
-            
-            esempio:
-                Se l'utente chiede giovani e adolescenti tu crei una lista in
-                param[0] equivalente a ['giovani', 'adolescenti']
-            
-        - l'id dell'organizzatore dell'evento
-        - rank dell'evento quindi la sua valutazione
-        
-        
+        Cerca eventi nel database in base a qualsiasi criterio espresso dall'utente.
+
+        Usa questo tool ogni volta che l'utente vuole trovare, scoprire, cercare
+        o chiedere informazioni su eventi, attività, iniziative o appuntamenti,
+        indipendentemente da come formula la richiesta.
+
+        I criteri di ricerca supportati sono:
+
+        - nome o titolo dell'evento
+        - data, periodo o orario in cui si svolge
+        - durata dell'evento
+        - luogo, città o sede dove si tiene
+        - descrizione o contenuto dell'evento
+        - tag o argomenti che riguardano l'evento
+        - target d'età del pubblico (preadolescenti, adolescenti, giovani, giovani_adulti)
+        - valutazione o popolarità dell'evento
+        - organizzatore dell'evento
+
+        L'utente può cercare per uno o più criteri contemporaneamente.
+        L'input del tool è la richiesta grezza dell'utente dalla quale
+        andranno estratti i parametri di ricerca.
     """
-    #! TOOL NON TERMINATO ################################################################################################
     pass
-    #! TOOL NON TERMINATO ################################################################################################
     
     # Content-Type = application/json
     BASE_URL = "https://strapi.brusegan.it"
@@ -58,15 +95,11 @@ def cerca_con_parametro(*param, cat):
         "giovani_adulti"
     ]
     
-    filtri_ricerca = param[0]
-    
     query_params = {
         "populate": "*", # permette di avere gli eventi con tutti i dettagli
     }
     
-    #popolamento dell'attributo filters[target][fascia][$in][{i}] in params con i valori corretti
-    for i, f in enumerate(LISTA_FASCE):
-        query_params[f"filters[target][fascia][$in][{i}]"] = f #
+    # utilizzando: filters[target][fascia][$in][{i}]
     
     query_headers = {
         "Content-Type": "application/json",
@@ -86,3 +119,6 @@ def cerca_con_parametro(*param, cat):
     
     
     return param
+
+def richiesta_eventi_server():
+    pass # funzione per la richiesta al server
